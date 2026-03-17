d2ffc4348084c1a1c85339b78bd11cabeacbad80
akluger123 <akluger@ssdsboston.net>
2026-03-17 13:45:24 -0400

    Add files via upload

---
+++ riddle.py
@@ -0,0 +1,7 @@
+print("BTW my name is ami")
+U = input(" Username: ")                   
+P = input(" Password: ")
+if U == "ami" and P == "oscar":
+        print(" congrats, tell me or email me <321cba> you get a prize")
+else:
+        print("incorrect login, try again")
